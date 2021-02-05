d3f25d3eeab00518b5146b58f15954c06843777d
acasax <acasax@gmail.com>
2021-02-05 23:51:00 +0100

    primer 1 finish

---
+++ primer1/php_vendor/footer.php
@@ -0,0 +1,119 @@
+<footer class="footer-wrap-layout1 section-shape1">
+    <div class="container">
+        <div class="footer-top-box">
+            <div class="row">
+                <div class="col-lg-3 col-sm-6 col-12">
+                    <div class="footer-box-layout1">
+                        <div class="footer-title">
+                            <h4>FEATURES</h4>
+                        </div>
+                        <div class="footer-menu-box">
+                            <ul class="footer-menu-list">
+                                <li>
+                                    <a href="#">Residential Services</a>
+                                </li>
+                                <li>
+                                    <a href="#">Commercial Services</a>
+                                </li>
+                                <li>
+                                    <a href="#">Vechile Wash</a>
+                                </li>
+                                <li>
+                                    <a href="#">Londry Facilities</a>
+                                </li>
+                                <li>
+                                    <a href="#">Carpet Removal</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6 col-12">
+                    <div class="footer-box-layout1">
+                        <div class="footer-title">
+                            <h4>COMPANY</h4>
+                        </div>
+                        <div class="footer-menu-box">
+                            <ul class="footer-menu-list">
+                                <li>
+                                    <a href="about1.php">About Us</a>
+                                </li>
+                                <li>
+                                    <a href="#">Testimonials</a>
+                                </li>
+                                <li>
+                                    <a href="#">Terms</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6 col-12">
+                    <div class="footer-box-layout1">
+                        <div class="footer-title">
+                            <h4>QUICK LINKS</h4>
+                        </div>
+                        <div class="footer-menu-box">
+                            <ul class="footer-menu-list">
+                                <li>
+                                    <a href="#">Features</a>
+                                </li>
+                                <li>
+                                    <a href="#">Pricing</a>
+                                </li>
+                                <li>
+                                    <a href="#">Partners</a>
+                                </li>
+                                <li>
+                                    <a href="#">Cloud Affiliate Program</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6 col-12">
+                    <div class="footer-box-layout1">
+                        <div class="footer-title">
+                            <h4>SIGN UP!</h4>
+                        </div>
+                        <div class="footer-newsletter">
+                            <p>Quuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+                            <div class="input-group stylish-input-group">
+                                <input type="text" class="form-control" placeholder="E-mail Address">
+                                <span class="input-group-addon">
+                                            <button type="submit">Submit</button>
+                                        </span>
+                            </div>
+                        </div>
+                        <div class="footer-social">
+                            <ul class="social-icon">
+                                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                                <li><a href="#"><i class="fab fa-pinterest"></i></a></li>
+                                <li><a href="#"><i class="fab fa-vimeo-v"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom-box">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="copyright">Copyright 2021. All Rights Reserved. Designe by <a href="www.resivoje.com">RešivoJe</a></div>
+                </div>
+                <div class="col-md-6">
+                    <div class="footer-bottom-menu">
+                        <ul>
+                            <li><a href="#">Terms of Service</a></li>
+                            <li><a href="#">Privacy Policy</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
