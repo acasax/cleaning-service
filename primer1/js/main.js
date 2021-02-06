b17e21f5ad2f97c45a94348e9ca0c29f771ca183
acasax <acasax@gmail.com>
2021-02-06 01:44:30 +0100

    primer2 finish

--- primer1/js/main.js
+++ primer1/js/main.js
@@ -153,7 +153,7 @@
     --------------------------------------*/
     if ($.fn.meanmenu) {
         $('nav#dropdown').meanmenu({
-            siteLogo: "<div class='mobile-menu-nav-back'><a class='logo-mobile' href='index.html'><img src='img/mobile-logo.png' alt='logo' class='img-fluid'/></a></div>"
+            siteLogo: "<div class='mobile-menu-nav-back'><a class='logo-mobile' href='index.php'><img src='img/mobile-logo.png' alt='logo' class='img-fluid'/></a></div>"
         });
     }
 
